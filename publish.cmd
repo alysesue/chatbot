6be230667eef974fc27c0bde75319be37718818b
AS <alyse.sue@gmail.com>
2018-07-20 15:28:10 +0800

    initCommit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=pandaiwebappbot-Web-Deploy.pubxml -p:Password=iNiKC3ZSdhyLs2Zd27fpDmEdNb2nMhYPEp9uxKHNR85Xm2K73vsM8JJjL5Cj
+
